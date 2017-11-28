65464a20c948b274251a7d10691b8bde9cf2a1b3
sushilcmd <sushil@realbox.in>
2017-11-28 13:10:34 +0530

    change appconfig

--- public/js/appConfig.js
+++ public/js/appConfig.js
@@ -1,12 +1,8 @@
 var ioUrl;
 var appUrl;
-var enablePassword;
 var isReady = 0
-var dataInCookies = {};
-var cookieKey = 'controlPanel';
 
-//App mode
-var env = 'LOCAL'; // LOCAL, STAGE, PROD
+var env = 'LOCAL';
 
 function setEnvironmentSpecifics(env) {
     switch (env) {
@@ -14,16 +10,7 @@ function setEnvironmentSpecifics(env) {
             ioUrl = 'http://10.0.0.186:5024';
             enablePassword = 0;
             break;
-        case 'STAGE':
-            ioUrl = 'http://52.163.63.199:5023';
-            enablePassword = 0;
-            break;
-        case 'PROD':
-            ioUrl = 'http://192.168.1.52:15000';
-            enablePassword = 1;
-            break;
     }
-    //common
     appUrl = ioUrl + '/services/';
 
 }
